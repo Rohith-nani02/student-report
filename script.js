e6932675f2d81047ad2c146564b936691e706d9e
Rohith-nani02 <rohithkalavalapudi@gmail.com>
2026-08-13 09:21:25 +0530

    Enhance showResult function to calculate grades
    Calculate percentage and assign grades based on marks.

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
 function showResult() {
-    document.getElementById("marks").innerHTML = "Marks: 85 - PASS";
+
+    let total = 419;
+    let maximum = 500;
+
+    let percentage = (total / maximum) * 100;
+
+    let grade;
+
+    if (percentage >= 90) {
+        grade = "A+";
+    }
+    else if (percentage >= 80) {
+        grade = "A";
+    }
+    else if (percentage >= 70) {
+        grade = "B";
+    }
+    else if (percentage >= 60) {
+        grade = "C";
+    }
+    else if (percentage >= 50) {
+        grade = "D";
+    }
+    else {
+        grade = "F";
+    }
+
+    document.getElementById("result").innerHTML =
+        "Result: PASS";
+
+    document.getElementById("percentage").innerHTML =
+        "Percentage: " + percentage.toFixed(2) + "%";
+
+    document.getElementById("grade").innerHTML =
+        "Grade: " + grade;
 }
